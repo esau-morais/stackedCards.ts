b8a3d9b4391145e1598da788669d6236fd50dafb
esau-morais <esaumorais7@gmail.com>
2023-01-16 14:53:41 -0300

    fix: update vite configuration and jest types

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,20 @@
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
 import react from '@vitejs/plugin-react-swc'
 import { defineConfig } from 'vite'
+import tsconfigPaths from 'vite-tsconfig-paths'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    host: '0.0.0.0',
-    port: 3000,
+  plugins: [react(), tsconfigPaths()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['./setupTests.ts'],
+    include: ['./src/**/*.spec.{ts,tsx}'],
+    watchExclude: [
+      '.*\\/node_modules\\/.*',
+      '.*\\/build\\/.*'
+    ],
   },
-  plugins: [react()],
 })
